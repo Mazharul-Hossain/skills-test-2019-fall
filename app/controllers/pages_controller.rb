0ddb7292ca0ccd3c4d2f6e79f77aa839912e0e7e
Mazharul Hossain <2561458+m-h@users.noreply.github.com>
2020-02-02 22:26:49 -0600

    Old stuff

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,8 +1,18 @@
 class PagesController < ApplicationController
 
+    def home
+    
+    end
+        
     def uofm_driven
         respond_to do |format|
             format.html { render :uofm_driven }
+        end
+    end
+
+    def uofm_cs
+        respond_to do |format|
+            format.html { render :uofm_cs }
         end
     end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  root to: 'pages#uofm_cs'
   get 'uofm-driven', to: 'pages#uofm_driven', as: 'uofm_driven'
+  get 'uofm-cs', to: 'pages#uofm_cs', as: 'uofm_cs'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
