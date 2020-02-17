29de02334dd7880f7cbb2d30f7cb1c3e50b8ea16
Mazharul Hossain <2561458+m-h@users.noreply.github.com>
2020-02-16 18:25:47 -0600

    s06a-submission

--- app/models/twitter_tweet.rb
+++ app/models/twitter_tweet.rb
@@ -1,2 +1,19 @@
+# == Schema Information
+#
+# Table name: twitter_tweets
+#
+#  id         :bigint           not null, primary key
+#  message    :text
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class TwitterTweet < ApplicationRecord
+    validate :message_140_char
+
+    def message_140_char
+        if message.length > 140
+            errors.add(:message, "message string must not be longer than 140 characters in length")
+        end
+    end
 end
--- test/models/twitter_tweet_test.rb
+++ test/models/twitter_tweet_test.rb
@@ -1,9 +1,23 @@
+# == Schema Information
+#
+# Table name: twitter_tweets
+#
+#  id         :bigint           not null, primary key
+#  message    :text
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 require 'test_helper'
 
 class TwitterTweetTest < ActiveSupport::TestCase
 
   test "twitter tweet cannot be more than 140 characters" do
-    assert false
+    one = twitter_tweets(:one)
+    assert one.valid?
+
+    two = twitter_tweets(:two)
+    assert_not two.valid?
   end
 
 end
