5654152b2a1502ae7833244e557e883d8cd52596
Mazharul Hossain <2561458+m-h@users.noreply.github.com>
2020-02-19 01:38:48 -0600

    skills-test-06b

--- app/models/twitter_tweet.rb
+++ app/models/twitter_tweet.rb
@@ -1,2 +1,13 @@
+# == Schema Information
+#
+# Table name: twitter_tweets
+#
+#  id         :bigint           not null, primary key
+#  message    :text
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class TwitterTweet < ApplicationRecord
+    validates :message, presence: true
 end
--- test/models/twitter_tweet_test.rb
+++ test/models/twitter_tweet_test.rb
@@ -1,9 +1,32 @@
+# == Schema Information
+#
+# Table name: twitter_tweets
+#
+#  id         :bigint           not null, primary key
+#  message    :text
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 require 'test_helper'
 
 class TwitterTweetTest < ActiveSupport::TestCase
 
   test "twitter tweet cannot be more than 140 characters" do
-    assert false
+    one = twitter_tweets(:one)
+    assert one.valid?
+
+    one.message = nil
+    assert_not one.valid?
+
+    one.message = ""
+    assert_not one.valid?
+
+    one.message = "          "
+    assert_not one.valid? 
+    
+    one.message = "\t"
+    assert_not one.valid?
   end
 
 end
